da56756cbedfe9f2352f2756d62badab2456d9c4
Roshan Singh <singh.roshan9718@gmail.com>
2024-01-03 20:37:33 +0530

    added categories screen

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,54 @@
+import { Pressable, View, Text, StyleSheet } from "react-native";
+
+function CategoryGridTile({ title, color }) {
+    return (
+        <View style={styles.gridItem}>
+            <Pressable android_ripple={{ color: '#ccc' }}
+                style={({ pressed }) => [styles.button, 
+                pressed ? styles.buttonPressed : null]}>
+
+                <View style={[styles.innerContainer, 
+                    { backgroundColor: color }]}>
+                    <Text style={styles.title}>
+                        {title}
+                    </Text>
+                </View>
+
+            </Pressable>
+        </View>
+    )
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        overflow: 'hidden',
+    },
+
+    button: {
+        flex: 1
+    },
+
+    buttonPressed: {
+        opacity: 0.5
+    },
+
+    innerContainer: {
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+});
